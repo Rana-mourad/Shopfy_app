b99848b880c13b85c9bd1cf7b4276ad6598ac64a
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-21 16:15:21 +0200

    change

--- shopfy/lib/views/Home/HomePage.dart
+++ shopfy/lib/views/Home/HomePage.dart
@@ -1,214 +1,107 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:get/get.dart';
 import 'package:provider/provider.dart';
-import 'package:shopfy/Model/productmodel.dart';
+import 'package:shopfy/Provider/AppAuthProvider.dart';
 import 'package:shopfy/Provider/ProductProvider.dart';
-import 'package:shopfy/theme/Appcolor.dart';
-import 'package:shopfy/theme/customcard.dart';
-import 'package:shopfy/theme/custometext.dart';
-import 'package:shopfy/widgets/buildimage.dart';
+import 'package:shopfy/Provider/category.provider.dart';
+import 'package:flexible_grid_view/flexible_grid_view.dart';
+import 'package:shopfy/widgets/Headline.dart';
+import 'package:shopfy/widgets/Product.dart';
+import 'package:shopfy/widgets/categories_row.home.widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final productProvider = Provider.of<ProductProvider>(context);
-
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomText(
-              text: "Categories",
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-            SizedBox(height: 20),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              clipBehavior: Clip.none,
-              child: Container(
-                height: 120,
-                child: Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/categories');
-                      },
-                      child: Column(
-                        children: [
-                          Container(
-                            width: 80,
-                            height: 80,
-                            child: SvgPicture.asset(
-                              "assets/images/arrow_right_s.svg",
-                              fit: BoxFit.scaleDown,
-                            ),
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.2),
-                                  offset: Offset(0, 8),
-                                  blurRadius: 15,
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          CustomText(
-                            text: "See More",
-                            fontSize: 15,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            CustomText(
-              text: "Latest",
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: Get.width,
-              height: 185,
-              clipBehavior: Clip.antiAlias,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Stack(
-                children: [
-                  Image.asset(
-                    "assets/images/Base.jpg",
-                    fit: BoxFit.cover,
-                    width: Get.width,
-                    height: 185,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomText(
-                          text: "For all your\nsummer clothing\nneeds",
-                          fontSize: 18,
-                          color: Colors.white,
-                          textAlign: TextAlign.start,
-                        ),
-                        SizedBox(height: 10),
-                        Container(
-                          width: 120,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(25),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.1),
-                                offset: Offset(0, 8),
-                                blurRadius: 15,
-                              ),
-                            ],
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              CustomText(text: "SEE MORE"),
-                              Container(
-                                width: 30,
-                                height: 30,
-                                decoration: BoxDecoration(
-                                  color: AppColor.primaryColor,
-                                  shape: BoxShape.circle,
-                                ),
-                                child: SvgPicture.asset(
-                                  "assets/images/arrow_right_s.svg",
-                                  fit: BoxFit.scaleDown,
-                                  color: Colors.white,
-                                ),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: 15),
-            Container(
-              height: 150,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: productProvider.products.length,
-                separatorBuilder: (BuildContext context, int index) {
-                  return SizedBox(width: 10);
-                },
-                itemBuilder: (BuildContext context, int index) {
-                  return _BuildProductItem(
-                    model: productProvider.products[index],
-                    onTap: () {},
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: 20),
-          ],
-        ),
-      ),
-    );
-  }
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _BuildProductItem extends StatelessWidget {
-  const _BuildProductItem({
-    Key? key,
-    required this.model,
-    required this.onTap,
-  }) : super(key: key);
-
-  final Product model;
-  final VoidCallback onTap;
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: CustomCard(
-        width: 130,
-        radius: 15,
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        alignment: Alignment.center,
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            BuildImage(
-              imageUrl: model.image!,
-              width: 80,
-              height: 80,
+            HeadlineWidget(title: 'Categories'),
+            Consumer<CategoryProvider>(
+              builder: (__, categoryProvider, _) {
+                return FutureBuilder(
+                  future: categoryProvider.getCategories(context, limit: 3),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.done) {
+                      if (snapshot.hasError) {
+                        return Text('Error While Get Data');
+                      } else if (snapshot.hasData) {
+                        return CategoriesRowHome(
+                          categories: snapshot.data ?? [],
+                        );
+                      } else {
+                        return Text('No Data Found');
+                      }
+                    } else {
+                      return Text(
+                          'Connection Statue ${snapshot.connectionState}');
+                    }
+                  },
+                );
+              },
             ),
-            CustomText(
-              text: model.title!,
-              maxLines: 1,
-              textAlign: TextAlign.start,
+            const SizedBox(
+              height: 20,
             ),
-            CustomText(
-              text: "\$${model.price!}",
-              maxLines: 1,
-              fontWeight: FontWeight.bold,
+            const HeadlineWidget(title: 'Latest'),
+            const SizedBox(
+              height: 10,
+            ),
+            const HeadlineWidget(title: 'Products'),
+            const SizedBox(
+              height: 10,
+            ),
+            Consumer<ProductProvider>(
+              builder: (__, productProvider, _) {
+                return FutureBuilder(
+                  future: productProvider.getProducts(context, limit: 3),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.done) {
+                      if (snapshot.hasError) {
+                        return Text('Error While Get Data');
+                      } else if (snapshot.hasData) {
+                        return FlexibleGridView(
+                          axisCount: GridLayoutEnum.threeElementsInRow,
+                          shrinkWrap: true,
+                          children: snapshot.data
+                                  ?.map((e) => ProductWidget(product: e))
+                                  .toList() ??
+                              [],
+                        );
+                      } else {
+                        return Text('No Data Found');
+                      }
+                    } else {
+                      return Text(
+                          'Connection Statue ${snapshot.connectionState}');
+                    }
+                  },
+                );
+              },
+            ),
+            ElevatedButton(
+              onPressed: () =>
+                  Provider.of<AppAuthProvider>(context, listen: false)
+                      .onLogout(context),
+              child: Text('Log Out'),
+            ),
+            ElevatedButton(
+              onPressed: () async {},
+              child: Text('Upload Image'),
             ),
           ],
         ),
