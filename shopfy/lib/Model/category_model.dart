f1433705ad6193d9795295f1aab5f9c2c7d668ed
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-21 16:11:19 +0200

    change

--- shopfy/lib/Model/category_model.dart
+++ shopfy/lib/Model/category_model.dart
@@ -1,21 +1,38 @@
-class Category {
+class CategoryData {
   String? id;
   String? title;
   String? description;
+  String? image;
+  int? shadowColor;
+  List<int>? colors;
+  DateTime? createdAt;
 
-  Category();
+  CategoryData();
 
-  Category.fromJson(Map<String, dynamic> json, String docId) {
+  CategoryData.fromJson(Map<String, dynamic> json, [String? docId]) {
     id = docId;
     title = json['title'];
     description = json['description'];
+    image = json['image'];
+    shadowColor = json['shadowColor'];
+    colors = json['colors'] != null
+        ? List<int>.from(json['colors'].map((e) => e))
+        : null;
+
+    createdAt = json['createdAt'] != null
+        ? DateTime.fromMillisecondsSinceEpoch(
+            json['createdAt'].millisecondsSinceEpoch)
+        : null;
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> categoryData = {};
-    categoryData['id'] = id;
-    categoryData['title'] = title;
-    categoryData['description'] = description;
-    return categoryData;
+    final Map<String, dynamic> data = {};
+    data['title'] = title;
+    data['description'] = description;
+    data['image'] = image;
+    data['shadowColor'] = shadowColor;
+    data['colors'] = colors;
+    data['createdAt'] = createdAt;
+    return data;
   }
 }
