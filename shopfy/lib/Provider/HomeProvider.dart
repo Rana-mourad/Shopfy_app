c9bfd2c45fe7c244fbf4d3982edda67e9a1f1c0d
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-24 10:46:02 +0200

    Home provider

--- shopfy/lib/Provider/HomeProvider.dart
+++ shopfy/lib/Provider/HomeProvider.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:quickalert/quickalert.dart';
 import 'package:shopfy/Model/adsmodel.dart';
-import 'package:shopfy/theme/collectionutils.dart';
 
 class HomeProvider extends ChangeNotifier {
   List<Ads>? adList;
@@ -10,13 +10,44 @@ class HomeProvider extends ChangeNotifier {
     await getAds();
   }
 
-  Future<void> getAds() async {
-    QuerySnapshot<Map<String, dynamic>> result = await FirebaseFirestore
-        .instance
-        .collection(CollectionsUtils.ads.name)
-        .get();
-    adList =
-        List<Ads>.from(result.docs.map((e) => Ads.fromJson(e.data(), e.id)));
-    notifyListeners();
+  Future<void> getAds({int? limit}) async {
+    try {
+      QuerySnapshot<Map<String, dynamic>>? result;
+      if (limit != null) {
+        result = await FirebaseFirestore.instance
+            .collection('ads')
+            .limit(limit)
+            .get();
+      } else {
+        result = await FirebaseFirestore.instance.collection('ads').get();
+      }
+
+      if (result.docs.isNotEmpty) {
+        adList = List<Ads>.from(
+          result.docs.map((e) => Ads.fromJson(e.data(), e.id)),
+        ).toList();
+
+        notifyListeners();
+      } else {
+        adList = [];
+        notifyListeners();
+      }
+    } catch (e) {
+      print('Error fetching ads: $e');
+    }
+  }
+
+  Future<void> showAlertDialog({
+    required BuildContext context,
+    required String title,
+    required String content,
+  }) async {
+    if (!context.mounted) return;
+    await QuickAlert.show(
+      context: context,
+      type: QuickAlertType.error,
+      title: title,
+      text: content,
+    );
   }
 }
