a474c27d5e1526ab9d964a11ef274c3d66add300
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-24 11:52:34 +0200

    Add ads

--- shopfy/lib/views/Home/HomePage.dart
+++ shopfy/lib/views/Home/HomePage.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopfy/Provider/AppAuthProvider.dart';
+import 'package:shopfy/Provider/HomeProvider.dart';
 import 'package:shopfy/Provider/ProductProvider.dart';
 import 'package:shopfy/Provider/category.provider.dart';
 import 'package:flexible_grid_view/flexible_grid_view.dart';
+import 'package:shopfy/widgets/Adswidget.dart';
 import 'package:shopfy/widgets/Headline.dart';
 import 'package:shopfy/widgets/Product.dart';
 import 'package:shopfy/widgets/categories_row.home.widget.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -28,31 +32,65 @@ class _HomePageState extends State<HomePage> {
             Consumer<CategoryProvider>(
               builder: (__, categoryProvider, _) {
                 return FutureBuilder(
-                  future: categoryProvider.getCategories(context, limit: 3),
+                    future: categoryProvider.getCategory(context, limit: 3),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.done) {
+                        if (snapshot.hasError) {
+                          return Text('Error While Get Data');
+                        } else if (snapshot.hasData) {
+                          return CategoriesRowHome(
+                            categories: snapshot.data ?? [],
+                          );
+                        } else {
+                          return Text('No Data Found');
+                        }
+                      } else {
+                        return Text(
+                            'Connection Statue ${snapshot.connectionState}');
+                      }
+                    });
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const HeadlineWidget(title: 'Ads'),
+            const SizedBox(
+              height: 10,
+            ),
+            Consumer<HomeProvider>(
+              builder: (__, homeProvider, _) {
+                return FutureBuilder(
+                  future: homeProvider.getAds(context, limit: 3),
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
                       return const CircularProgressIndicator();
                     } else if (snapshot.connectionState ==
                         ConnectionState.done) {
                       if (snapshot.hasError) {
-                        return Text('Error While Get Data');
+                        return Text('Error While Get Ads Data');
                       } else if (snapshot.hasData) {
-                        return CategoriesRowHome(
-                          categories: snapshot.data ?? [],
+                        return FlexibleGridView(
+                          axisCount: GridLayoutEnum.threeElementsInRow,
+                          shrinkWrap: true,
+                          children: snapshot.data
+                                  ?.map((e) => AdWidget(ad: e))
+                                  .toList() ??
+                              [],
                         );
                       } else {
-                        return Text('No Data Found');
+                        return Text('No Ads Data Found');
                       }
                     } else {
                       return Text(
-                          'Connection Statue ${snapshot.connectionState}');
+                          'Connection State ${snapshot.connectionState}');
                     }
                   },
                 );
               },
-            ),
-            const SizedBox(
-              height: 20,
             ),
             const HeadlineWidget(title: 'Latest'),
             const SizedBox(
@@ -65,44 +103,60 @@ class _HomePageState extends State<HomePage> {
             Consumer<ProductProvider>(
               builder: (__, productProvider, _) {
                 return FutureBuilder(
-                  future: productProvider.getProducts(context, limit: 3),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const CircularProgressIndicator();
-                    } else if (snapshot.connectionState ==
-                        ConnectionState.done) {
-                      if (snapshot.hasError) {
-                        return Text('Error While Get Data');
-                      } else if (snapshot.hasData) {
-                        return FlexibleGridView(
-                          axisCount: GridLayoutEnum.threeElementsInRow,
-                          shrinkWrap: true,
-                          children: snapshot.data
-                                  ?.map((e) => ProductWidget(product: e))
-                                  .toList() ??
-                              [],
-                        );
+                    future: productProvider.getProducts(context, limit: 3),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.done) {
+                        if (snapshot.hasError) {
+                          return Text('Error While Get Data');
+                        } else if (snapshot.hasData) {
+                          return FlexibleGridView(
+                            axisCount: GridLayoutEnum.threeElementsInRow,
+                            shrinkWrap: true,
+                            children: snapshot.data
+                                    ?.map((e) => ProductWidget(product: e))
+                                    .toList() ??
+                                [],
+                          );
+                        } else {
+                          return Text('No Data Found');
+                        }
                       } else {
-                        return Text('No Data Found');
+                        return Text(
+                            'Connection Statue ${snapshot.connectionState}');
                       }
-                    } else {
-                      return Text(
-                          'Connection Statue ${snapshot.connectionState}');
-                    }
-                  },
-                );
+                    });
               },
             ),
             ElevatedButton(
-              onPressed: () =>
-                  Provider.of<AppAuthProvider>(context, listen: false)
-                      .onLogout(context),
-              child: Text('Log Out'),
-            ),
+                onPressed: () =>
+                    Provider.of<AppAuthProvider>(context, listen: false)
+                        .onLogout(context),
+                child: Text('LogOut')),
             ElevatedButton(
-              onPressed: () async {},
-              child: Text('Upload Image'),
-            ),
+                onPressed: () async {
+                  FilePickerResult? result =
+                      await FilePicker.platform.pickFiles(
+                    withData: true,
+                    type: FileType.image,
+                  );
+                  var refrence = FirebaseStorage.instance
+                      .ref('products/${result?.files.first.name}');
+
+                  if (result?.files.first.bytes != null) {
+                    var uploadResult = await refrence.putData(
+                        result!.files.first.bytes!,
+                        SettableMetadata(contentType: 'image/png'));
+
+                    if (uploadResult.state == TaskState.success) {
+                      print(
+                          '>>>>>>>>>>>>>>>>${await refrence.getDownloadURL()}');
+                    }
+                  }
+                },
+                child: Text('upload image')),
           ],
         ),
       ),
