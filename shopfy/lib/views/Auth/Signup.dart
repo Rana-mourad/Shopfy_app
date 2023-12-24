7db01ea6bb068f0f8fdff68f5a46e1c52390239c
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-24 10:50:07 +0200

    simple modfy

--- shopfy/lib/views/Auth/Signup.dart
+++ shopfy/lib/views/Auth/Signup.dart
@@ -1,43 +1,22 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:quickalert/models/quickalert_type.dart';
-import 'package:quickalert/widgets/quickalert_dialog.dart';
 import 'package:shopfy/Provider/AppAuthProvider.dart';
 import 'package:shopfy/views/Auth/Login.dart';
 import 'package:shopfy/views/Auth/paswordforgot.dart';
-import 'package:shopfy/views/Home/HomePage.dart';
 
-class SignUpPage extends StatefulWidget {
-  const SignUpPage({Key? key}) : super(key: key);
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
 
   @override
-  State<SignUpPage> createState() => _SignUpPageState();
+  State<SignupPage> createState() => _SignupPageState();
 }
 
-class _SignUpPageState extends State<SignUpPage> {
-  late TextEditingController emailController;
-  late TextEditingController userController;
-  late TextEditingController passwordController;
-  bool obscureText = true;
-  bool isChecked = false;
-  late AppAuthProvider signUpProvider;
-
+class _SignupPageState extends State<SignupPage> {
   @override
   void initState() {
-    signUpProvider = AppAuthProvider();
-    emailController = TextEditingController();
-    userController = TextEditingController();
-    passwordController = TextEditingController();
+    Provider.of<AppAuthProvider>(context, listen: false).init();
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    signUpProvider.dispose();
-    emailController.dispose();
-    userController.dispose();
-    passwordController.dispose();
-    super.dispose();
   }
 
   @override
@@ -46,241 +25,176 @@ class _SignUpPageState extends State<SignUpPage> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: ChangeNotifierProvider.value(
-            value: signUpProvider,
-            child: Consumer<AppAuthProvider>(
-              builder: (context, signUpProvider, _) {
-                return Form(
-                  key: signUpProvider.formKey,
-                  child: SingleChildScrollView(
-                    child: SafeArea(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => LoginPage(),
-                                    ),
-                                  );
-                                },
-                                child: Text(
-                                  'Login',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.blue,
+          child: Consumer<AppAuthProvider>(
+            builder: (ctx, appAuthProvider, _) {
+              return Form(
+                key: appAuthProvider.formKey,
+                child: SingleChildScrollView(
+                  child: SafeArea(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => LoginPage(),
                                   ),
-                                ),
-                              ),
-                              Text(
-                                ' | ',
+                                );
+                              },
+                              child: Text(
+                                'Login',
                                 style: TextStyle(
                                   fontSize: 14,
                                   fontWeight: FontWeight.w500,
                                   color: Colors.blue,
                                 ),
                               ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => AuthPage(),
-                                    ),
-                                  );
-                                },
-                                child: Text(
-                                  'Forgot Password',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.blue,
+                            ),
+                            Text(
+                              ' | ',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.blue,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => AuthPage(),
                                   ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            'Sign Up',
-                            style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF515C6F),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: InputDecoration(
-                              labelText: 'Email',
-                              suffixIcon: const Icon(
-                                Icons.mail,
-                                color: Color(0xFF727C8E),
-                              ),
-                              labelStyle: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xFF515C6F),
-                              ),
-                              isDense: true,
-                              filled: true,
-                              fillColor: Color.fromARGB(255, 230, 233, 240),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          TextFormField(
-                            controller: userController,
-                            decoration: InputDecoration(
-                              labelText: 'User',
-                              suffixIcon: const Icon(
-                                Icons.person,
-                                color: Color(0xFF727C8E),
-                              ),
-                              labelStyle: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xFF515C6F),
-                              ),
-                              isDense: true,
-                              filled: true,
-                              fillColor: Color.fromARGB(255, 230, 233, 240),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          TextFormField(
-                            obscureText: signUpProvider.obscureText,
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              suffixIcon: InkWell(
-                                onTap: () {
-                                  signUpProvider.toggleObscure();
-                                },
-                                child: signUpProvider.obscureText
-                                    ? const Icon(
-                                        Icons.visibility_off,
-                                        color: Color(0xFF727C8E),
-                                      )
-                                    : const Icon(
-                                        Icons.visibility,
-                                        color: Color(0xFF727C8E),
-                                      ),
-                              ),
-                              labelStyle: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xFF515C6F),
-                              ),
-                              isDense: true,
-                              filled: true,
-                              fillColor: Color.fromARGB(255, 230, 233, 240),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            children: [
-                              Checkbox(
-                                value: isChecked,
-                                onChanged: (value) {
-                                  false;
-                                },
-                                activeColor: Color(0xFF6969FF),
-                              ),
-                              Text(
-                                'I agree to receive offers',
+                                );
+                              },
+                              child: Text(
+                                'Forgot Password',
                                 style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w300,
-                                  color: Color(0xFF515C6F),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.blue,
                                 ),
                               ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          ElevatedButton(
-                            onPressed: () async {
-                              await signUpProvider.signUp(context);
-                              await Future.delayed(const Duration(seconds: 2));
-                              QuickAlert.show(
-                                context: context,
-                                type: QuickAlertType.success,
-                                text: 'Signup successful!',
-                              );
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => HomePage(),
-                                ),
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Color(0xFFFF6969),
-                              minimumSize: const Size(300, 60),
                             ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'SignUp',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                const SizedBox(width: 10),
-                                Icon(Icons.chevron_right, color: Colors.white),
-                              ],
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF515C6F),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'User',
+                            suffixIcon: const Icon(
+                              Icons.person_off_outlined,
+                              color: Color(0xFF727C8E),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            'By creating an account, you agree to our Terms of Service and Privacy Policy',
-                            style: TextStyle(
+                            labelStyle: TextStyle(
                               fontSize: 12,
-                              fontWeight: FontWeight.w300,
+                              fontWeight: FontWeight.w500,
                               color: Color(0xFF515C6F),
                             ),
+                            isDense: true,
+                            filled: true,
+                            fillColor: Color.fromARGB(255, 230, 233, 240),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
-                          const SizedBox(
-                            height: 20,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: appAuthProvider.emailController,
+                          validator: (value) {
+                            if (value == null || value == '') {
+                              return 'email is required';
+                            }
+                            if (!EmailValidator.validate(value)) {
+                              return 'Enter Valid Email';
+                            } else {
+                              if (!value.split('@').last.contains('gmail')) {
+                                return 'Enter Valid Gmail';
+                              }
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            label: const Text('Email'),
+                            suffixIcon: const Icon(Icons.mail),
+                            // fillColor: Colors.red,
+                            isDense: true,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15)),
                           ),
-                        ],
-                      ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          obscureText: appAuthProvider.obscureText,
+                          controller: appAuthProvider.passwordController,
+                          validator: (value) {
+                            if (value == null || value == '') {
+                              return 'password is required';
+                            }
+                            if (value.length < 8) {
+                              return 'Password length must be 8';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            label: const Text('Password'),
+                            suffixIcon: InkWell(
+                              onTap: () {
+                                appAuthProvider.toggleObscure();
+                              },
+                              child: appAuthProvider.obscureText
+                                  ? const Icon(Icons.visibility_off)
+                                  : const Icon(Icons.visibility),
+                            ),
+                            isDense: true,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            await appAuthProvider.signUp(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(300, 60),
+                          ),
+                          child: const Text('Signup'),
+                        )
+                      ],
                     ),
                   ),
-                );
-              },
-            ),
+                ),
+              );
+            },
           ),
         ),
       ),
