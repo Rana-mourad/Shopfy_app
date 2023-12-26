266d8576850d5fa4ecb9724036cb5e3feedba52b
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-26 05:02:10 +0200

    small change

--- shopfy/lib/Provider/HomeProvider.dart
+++ shopfy/lib/Provider/HomeProvider.dart
@@ -3,33 +3,33 @@ import 'package:flutter/material.dart';
 import 'package:quickalert/quickalert.dart';
 import 'package:shopfy/Model/adsmodel.dart';
 
-class HomeProvider extends ChangeNotifier {
-  List<Ads>? adList;
-}
+class HomeProvider {
+  Future<List<Ads>?> getAds(BuildContext context, {int? limit}) async {
+    try {
+      QuerySnapshot<Map<String, dynamic>>? result;
+      if (limit != null) {
+        result = await FirebaseFirestore.instance
+            .collection('Ads')
+            .limit(limit)
+            .get();
+      } else {
+        result = await FirebaseFirestore.instance.collection('Ads').get();
+      }
 
-Future<List<Ads>?> getAds(BuildContext context, {int? limit}) async {
-  try {
-    QuerySnapshot<Map<String, dynamic>>? result;
-    if (limit != null) {
-      result =
-          await FirebaseFirestore.instance.collection('Ads').limit(limit).get();
-    } else {
-      result = await FirebaseFirestore.instance.collection('Ads').get();
+      if (result.docs.isNotEmpty) {
+        var AdsList =
+            List<Ads>.from(result.docs.map((e) => Ads.fromJson(e.data(), e.id)))
+                .toList();
+
+        return AdsList;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      if (!context.mounted) return null;
+      await QuickAlert.show(
+          context: context, type: QuickAlertType.error, title: e.toString());
+      return null;
     }
-
-    if (result.docs.isNotEmpty) {
-      var AdsList =
-          List<Ads>.from(result.docs.map((e) => Ads.fromJson(e.data(), e.id)))
-              .toList();
-
-      return AdsList;
-    } else {
-      return [];
-    }
-  } catch (e) {
-    if (!context.mounted) return null;
-    await QuickAlert.show(
-        context: context, type: QuickAlertType.error, title: e.toString());
-    return null;
   }
 }
