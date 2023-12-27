d37fe64852d9a5b2249e387d1e38b3048ee6afb2
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-27 09:38:59 +0200

    appbar

--- shopfy/lib/widgets/appbar.dart
+++ shopfy/lib/widgets/appbar.dart
@@ -1,23 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shopfy/theme/colorsutil.dart';
+import 'package:shopfy/widgets/Cart_widget.dart';
 
 class AppBarEx {
   static PreferredSizeWidget get getAppBar => AppBar(
         surfaceTintColor: Colors.white,
         actions: [
-          Stack(
-            children: [
-              IconButton(
-                  onPressed: () {},
-                  icon: Image.asset('assets/images/Messages.svg')),
-              Positioned(
-                  bottom: 6,
-                  child: Badge(
-                    backgroundColor: ColorsUtil.badgeColor,
-                    label: Text('5'),
-                  ))
-            ],
-          ),
+          const CartBadgeWidget(),
           Stack(
             children: [
               IconButton(
